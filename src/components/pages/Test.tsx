46bba219de36c4e0ac06a301f27969d1b3ba1b64
kotahorii <khorii.micron@gmail.com>
2021-11-24 23:16:47 +0900

    hotpepper component created

--- src/components/pages/Test.tsx
+++ src/components/pages/Test.tsx
@@ -1,16 +1,15 @@
 import { Layout } from 'components/templates/Layout'
 import { useTest } from 'hooks/useTest'
 import { VFC } from 'react'
+import { useQueryClient } from 'react-query'
+import { HotPepperQueryType } from 'types/types'
 
 export const Test: VFC = () => {
-  const {
-    rakutenData,
-    keyword,
-    keywordChange,
-    refetchData,
-    isError,
-    postHotPepperParams,
-  } = useTest()
+  const { rakutenData, keyword, keywordChange, refetchData, isError } =
+    useTest()
+  const queryClient = useQueryClient()
+  const hotPepperData =
+    queryClient.getQueryData<HotPepperQueryType>('hotPepper')
   return (
     <Layout>
       <div className="flex flex-row space-x-2">
@@ -28,19 +27,48 @@ export const Test: VFC = () => {
           submit
         </button>
       </div>
-      {encodeURI(keyword)}
-      <div className="grid grid-cols-2">
-        {isError ? (
-          <p>データが存在しませんでした。</p>
-        ) : (
-          <ul className="flex flex-col space-y-2">
-            {rakutenData?.map((rakuten) => (
-              <li key={rakuten.hotel[0].hotelBasicInfo?.hotelNo}>
-                {rakuten.hotel[0].hotelBasicInfo?.hotelName}
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className="grid grid-cols-2 w-full space-x-3 pt-5">
+        <div className="flex flex-col items-center bg-green-100 rounded-lg w-full">
+          {isError ? (
+            <p className="text-lg mt-10 ">データが存在しませんでした。</p>
+          ) : (
+            <ul className="flex flex-col space-y-2 min-h-full p-2">
+              {rakutenData?.map((rakuten) => (
+                <li
+                  className="bg-green-50 p-3 text-gray-500 font-semibold rounded-lg w-full h-32"
+                  key={rakuten.hotel[0].hotelBasicInfo?.hotelNo}
+                >
+                  <a className="" href="">
+                    {rakuten.hotel[0].hotelBasicInfo?.hotelName}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="flex flex-col bg-green-200 items-center rounded-lg">
+          {hotPepperData?.length === 0 ? (
+            <p className="text-lg mt-10">データが存在しませんでした。</p>
+          ) : (
+            <ul className="flex flex-col space-y-2 w-full min-h-screen p-2">
+              {hotPepperData?.map((hotPepper) => (
+                <li
+                  className="bg-green-50 p-3 text-gray-500 font-semibold rounded-lg w-full h-32"
+                  key={hotPepper.id}
+                >
+                  <a
+                    className="p-2 rounded-lg hover:bg-gray-200"
+                    href={hotPepper.urls.pc}
+                  >
+                    {hotPepper.name}
+                  </a>
+                  <p>{hotPepper.address}</p>
+                  <p>{hotPepper.genre.name}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </Layout>
   )
--- src/hooks/useMutateHotPepper.ts
+++ src/hooks/useMutateHotPepper.ts
@@ -1,11 +1,9 @@
 import axios from 'axios'
-import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { useMutation, useQueryClient } from 'react-query'
 import { HotPepperQueryType, HotPepperRes } from 'types/types'
 
 export const useMutateHotPepper = () => {
   const queryClient = useQueryClient()
-  const previousData = queryClient.getQueryData<HotPepperQueryType>('hotPepper')
-  useQuery({ queryKey: 'hotPepper' })
   const postHotPepperParams = useMutation(
     (key: string) =>
       axios.post<HotPepperRes>('http://127.0.0.1:8000/api/v1/hotpeppers', {
@@ -13,9 +11,13 @@ export const useMutateHotPepper = () => {
       }),
     {
       onSuccess: (res) => {
-        queryClient.setQueryData<HotPepperQueryType>('hotPepper', [
-          ...res.data.results.shop,
-        ])
+        if (!res.data.results.shop) {
+          queryClient.setQueryData('hotPepper', [])
+        } else {
+          queryClient.setQueryData<HotPepperQueryType>('hotPepper', [
+            ...res.data.results.shop,
+          ])
+        }
       },
     }
   )
