06f2c3941ecc0000df60f12703172d5f76874713
kotahorii <khorii.micron@gmail.com>
2021-11-24 14:53:26 +0900

    custom hook created

--- src/hooks/useQueryHotPepper.ts
+++ src/hooks/useQueryHotPepper.ts
@@ -1,4 +1,17 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { HotPepperRes } from 'types/types'
 
-export const useQueryHotPepper = () => {
-  return 
+const getHotPepperData = async (keyword: string) => {
+  const { data } = await axios.get<HotPepperRes>(
+    `${process.env.REACT_APP_HOTPEPPER_URL}${keyword}`
+  )
+  return data
+}
+export const useQueryHotPepperData = (keyword: string) => {
+  return useQuery<HotPepperRes, Error>({
+    queryKey: 'hotPepper',
+    queryFn: () => getHotPepperData(keyword),
+    enabled: false,
+  })
 }
--- src/hooks/useQueryRakuten.ts
+++ src/hooks/useQueryRakuten.ts
@@ -1,3 +1,17 @@
-export const useQueryRakuten = () => {
-  return
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { RakutenRes } from 'types/types'
+
+const getRakutenData = async (keyword: string) => {
+  const { data } = await axios.get<RakutenRes>(
+    `${process.env.REACT_APP_RAKUTEN_URL}${keyword}`
+  )
+  return data
+}
+export const useQueryRakutenData = (keyword: string) => {
+  return useQuery<RakutenRes, Error>({
+    queryKey: 'rakuten',
+    queryFn: () => getRakutenData(keyword),
+    enabled: false,
+  })
 }
